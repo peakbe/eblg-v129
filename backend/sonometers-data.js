ef7bbcdadc45b69e5947f4294b1f0b309bfc2a5f
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-05-21 15:38:54 +0200

    Update sonometers-data.js

--- backend/sonometers-data.js
+++ backend/sonometers-data.js
@@ -4,22 +4,167 @@
 // ======================================================
 
 const SONOMETERS = [
-    { id: "F017", address: "Rue de la Pommeraie, 4690 Wonck, Belgique", lat: 50.764883, lon: 5.630606 },
-    { id: "F001", address: "Rue Franquet, Houtain", lat: 50.738044, lon: 5.608833 },
-    { id: "F014", address: "Rue Léon Labaye, Juprelle", lat: 50.718894, lon: 5.573164 },
-    { id: "F015", address: "Rue du Brouck, Juprelle", lat: 50.688839, lon: 5.526217 },
-    { id: "F005", address: "Rue Caquin, Haneffe", lat: 50.639331, lon: 5.323519 },
-    { id: "F003", address: "Rue Fond Méan, Saint-Georges", lat: 50.601167, lon: 5.381400 },
-    { id: "F011", address: "Rue Albert 1er, Saint-Georges", lat: 50.601142, lon: 5.356006 },
-    { id: "F008", address: "Rue Warfusée, Saint-Georges", lat: 50.594878, lon: 5.358950 },
-    { id: "F002", address: "Rue Noiset, Saint-Georges", lat: 50.588414, lon: 5.370522 },
-    { id: "F007", address: "Rue Yernawe, Saint-Georges", lat: 50.590756, lon: 5.344114 },
-    { id: "F009", address: "Bibliothèque Communale, Place Verte 4470 Stockay", lat: 50.580831, lon: 5.355417 },
-    { id: "F004", address: "Vinâve des Stréats, Verlaine", lat: 50.605414, lon: 5.321406 },
-    { id: "F010", address: "Rue Haute Voie, Verlaine", lat: 50.599392, lon: 5.313492 },
-    { id: "F013", address: "Rue Bois Léon, Verlaine", lat: 50.586914, lon: 5.308678 },
-    { id: "F016", address: "Rue de Chapon-Seraing, Verlaine", lat: 50.619616, lon: 5.295344 },
-    { id: "F006", address: "Rue Bolly Chapon, Seraing", lat: 50.609594, lon: 5.271403 },
-    { id: "F012", address: "Rue Barbe d'Or, 4317 Aineffe", lat: 50.621917, lon: 5.254747 }
+    { 
+id: F017,
+name: "F017",
+lat: 50.764883,
+lon: 5.630606,
+db: 42,
+address: "Rue de la Pommeraie",
+town: "Wonck",
+status: "OK"},
+
+    {
+id: F001,
+name: "F001",
+lat: 50.738044,
+lon: 5.608833,
+db: 42,
+address: "Rue Franquet",
+town: "Houtain",
+status: "OK"}
+
+    {
+id: F014,
+name: "F014",
+lat: 50.718894,
+lon: 5.573164,
+db: 42,
+address: "Rue Léon Labaye",
+town: "Juprelle",
+status: "OK"}
+
+    {
+id: F015,
+name: "F015",
+lat: 50.688839,
+lon: 5.526217,
+    db: 46,
+address: "Rue du Brouck,
+town: "Juprelle",
+status: "OK"},
+
+    {
+id: "F005",
+name: "F005",
+lat: 50.639331,
+lon: 5.323519,  
+    db: 46,
+address: "Rue Caquin", 
+town: "Haneffe",
+status: "OK"},
+    
+    {
+id: "F003", 
+name: "F003",
+lat: 50.601167,
+lon: 5.381400,
+    db: 46,
+address: "Rue Fond Méan",
+town: "Saint-Georges",
+status: "OK"},
+
+
+    { id: "F011",
+        name: "F001",
+        lat: 50.601142, 
+        lon: 5.356006,
+        db: 46,
+        address: "Rue Albert 1er", 
+        town: "Saint-Georges",
+        status: "OK"},
+
+    { id: "F008", 
+        name: "F008",
+        lat: 50.594878,
+        lon: 5.358950,
+        db: 46,v
+        address: "Rue Warfusée", 
+        town: "Saint-Georges",
+        status: "OK"},
+
+    { id: "F002",
+        name: "F002",
+        lat: 50.588414,
+        lon: 5.370522,
+        db: 46,
+        address: "Rue Noiset", 
+        town: "Saint-Georges",
+        status: "OK"},
+
+    { id: "F007",
+        name: "F007",
+        lat: 50.590756,
+        lon: 5.344114,
+        db: 46,
+        address: "Rue Yernawe", 
+        town: "Saint-Georges",
+        status: "OK"},
+
+
+    { id: "F009",
+        name: "F009",
+        lat: 50.580831,
+        lon: 5.355417,
+        db: 46,
+        address: "Bibliothèque Communale, Place Verte", 
+        town: "Stockay",
+        status:  "OK"},
+
+    { id: "F004",
+        name: "F004",
+        lat: 50.605414,
+        lon: 5.321406,
+        db: 46,
+        address: "Vinâve des Stréats", 
+        town: "Verlaine",
+        status: "OK"},
+        
+    { id: "F010",
+        name: "F010",
+        lat: 50.599392,
+        lon: 5.313492,
+        db: 46,
+        address: "Rue Haute Voie",
+        town: "Verlaine",
+        status: "OK"}
+
+    { id: "F013",
+        name: "F013",
+        lat: 50.586914,
+        lon: 5.308678,
+        db: 46,
+        address: "Rue Bois Léon", 
+        town: "Verlaine",
+        status: "OK"}
+
+
+    { id: "F016",
+        name: "F016",
+        lat: 50.619616,
+        lon: 5.295344,
+        db: 46,
+        address: "Rue de Chapon-Seraing", 
+        town: "Verlaine",
+        status: "OK"}
+
+
+    { id: "F006",
+        name: "F006",
+        lat: 50.609594,
+        lon: 5.271403
+         db: 46,
+        address: "Rue Bolly"
+        town: "Chapon-Seraing",
+        status: "OK"}
+
+
+    { id: "F012",
+        name: "F012",
+        lat: 50.621917,
+        lon: 5.254747,
+        db: 48,
+        address: "Rue Barbe d'Or", 
+        town: "Aineffe",  }
 ];
     export default SONOMETERS;
